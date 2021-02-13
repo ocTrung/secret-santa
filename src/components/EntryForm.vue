2999d77f621eafdffeedc8cd6c1f9d9a7ba9791f
ocTrung <t.nguyenvo@eagles.oc.edu>
2021-02-13 17:13:26 -0600

    commit 2

---
+++ src/components/EntryForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+        <form class="review-form" @submit.prevent="onSubmit">
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+      
+      <p>
+        <label for="email">Email:</label>      
+        <textarea id="email" v-model="email"></textarea>
+      </p>
+          
+      <p>
+        <input type="submit" value="Submit">  
+      </p>    
+    
+    </form>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      name: null,
+      email: null,
+      }
+    },
+    methods: {
+      submitForm(){
+        axios.post(/, this.form)
+          .then((res) => {
+
+          })
+          .catch((error) => {
+
+          }).finally(() => {
+            
+          })
+      }
+    }
+
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
